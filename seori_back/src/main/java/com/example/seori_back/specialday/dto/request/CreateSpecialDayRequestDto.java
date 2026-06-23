0ded1b52cbd5f7f4968dac4b916fab34eaeab38c
zlon <zlon@outlook.kr>
2026-06-23 17:33:56 +0900

    feat: specialDay name 길이 검증추가

--- seori_back/src/main/java/com/example/seori_back/specialday/dto/request/CreateSpecialDayRequestDto.java
+++ seori_back/src/main/java/com/example/seori_back/specialday/dto/request/CreateSpecialDayRequestDto.java
@@ -2,11 +2,12 @@ package com.example.seori_back.specialday.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
 public record CreateSpecialDayRequestDto(
         @NotNull LocalDate date,
-        @NotBlank String name,
+        @NotBlank @Size(max = 30) String name,
         boolean recurring
 ) {}
