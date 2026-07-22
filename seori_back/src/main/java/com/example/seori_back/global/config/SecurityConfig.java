7f8d6b9ccddc3fd7243c316e2bded5cc330181d3
zlon <zlon@outlook.kr>
2026-07-22 16:32:10 +0900

    feature: errorResponse 경로 수정

--- seori_back/src/main/java/com/example/seori_back/global/config/SecurityConfig.java
+++ seori_back/src/main/java/com/example/seori_back/global/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5174"));
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
